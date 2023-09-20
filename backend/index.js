06fe53bf1673aa1b6c3350c8dcdae6b02be53a8f
Anderson <andersonordonez455@gmail.com>
2023-09-20 12:06:49 -0500

    Conexion a la base de datos

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
+// Al momento de poner el "type module" se debe ahora hacer la sintaxis de js que es importando, en este caso, express
+import conectarDB from './config/db.js';
+import dotenv from 'dotenv';
 
 const app = express();
 
+dotenv.config();
+conectarDB();
+
+const PORT = process.env.PORT || 4000;
 app.listen(4000, () => {
-    console.log('Desde el puerto 4000')
+    console.log(`Desde el puerto ${PORT}`);
 })
